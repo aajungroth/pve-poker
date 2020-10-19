4cf5eaaeb418251da9fa18284c9dac74db5559ac
aajungroth <aajungroth@ucdavis.edu>
2020-10-18 20:26:32 -0700

    Add action panel component

---
+++ src/components/ActionPanel.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="action-box">
+    <h1>
+      Actions
+    </h1>
+    <button>
+      Check
+    </button>
+    <button>
+      Bet
+    </button>
+    <button>
+      Call
+    </button>
+    <label>
+      Raise Amount
+    </label>
+    <input type="number" min="0" max="1000000"/>
+    <button>
+      Rasie
+    </button>
+    <button>
+      Rerasie
+    </button>
+    <button>
+      Fold
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActionPanel',
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+  .action-box {
+    border-color: #0000FF;
+    border-style: solid;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
