986663dcf377b617c19203f39ddbd37e11e562c6
aajungroth <aajungroth@ucdavis.edu>
2020-11-11 20:07:59 -0800

    Add gold view component

---
+++ src/components/GoldView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gold-view">
+    <h1>
+     Gold View
+    </h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GoldView',
+    props: {},
+    data() {
+      return {};
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+  .gold-view {
+    border-color: #D4AF37;
+    border-style: solid;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
