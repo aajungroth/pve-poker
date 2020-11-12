803beeb6c14c3eb19653009196fb5dda4da3ac3f
aajungroth <aajungroth@ucdavis.edu>
2020-11-11 20:08:32 -0800

    Write simulation view component

---
+++ src/components/SimulationView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="simulation-view">
+    <h1>
+      Simulation View
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SimulationView',
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+   .simulation-view {
+     border-color: #00FF00;
+     border-style: solid;
+     display: flex;
+     flex-direction: column;
+   }
+</style>
